ba87e3c4c4a931f1852d92d9628fe7c973296701
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-05-15 16:52:48 +0000

    vite setup

--- src/jest.setup.ts
+++ src/jest.setup.ts
@@ -1,15 +1,18 @@
 import { TextEncoder, TextDecoder } from 'util';
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder as any;
+import { JSDOM } from 'jsdom';
 
-     global.TextEncoder = TextEncoder;
-     global.TextDecoder = TextDecoder as any;
+const dom = new JSDOM('<!DOCTYPE html><html><body></body></html>');
+global.document = dom.window.document;
+global.window = dom.window as any;
 
-     // Extend global type to include Chart
-     declare global {
-       // eslint-disable-next-line no-var
-       var Chart: jest.Mock;
-     }
+declare global {
+  var Chart: jest.Mock;
+}
 
-     // Mock Chart.js
-     global.Chart = jest.fn().mockImplementation(() => ({
-       destroy: jest.fn(),
-     }));
+global.Chart = jest.fn().mockImplementation((ctx, config) => ({
+  destroy: jest.fn(),
+  update: jest.fn(),
+  data: config.data,
+}));
